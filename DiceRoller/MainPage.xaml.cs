c3010715f2d23cf12769c008aeb6b3ba109f43a4
Vinicius Coelho Gama <128941564+ViniciusCgama@users.noreply.github.com>
2023-09-26 13:43:02 -0300

    Initial commit

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    //Sortear um número aleatório através do clique do botão
+    //Devolver o número sorteado para a interface
+    //Conseguir selecionar a quantidade de lados a partir do Picker
+    private void SortearNumeroAleatorio(object sender, EventArgs e)
+    {
+        var maxValue = SeletorDeLadosDoDado.SelectedItem;
+        var numeroSorteado = new Random().Next(1, (int)maxValue + 1);
+
+        NumeroSorteado.Text = numeroSorteado.ToString();
+    }
+
+}
+
